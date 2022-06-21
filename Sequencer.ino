c4a1fc74e831a59065f70b309f94f15ae1c1d326
kellerdevsource <kellerdevgames@gmail.com>
2022-06-21 21:29:02 +0300

    Update Sequencer.ino

--- Sequencer.ino
+++ Sequencer.ino
@@ -1,4 +1,10 @@
 
+uint32_t newTime;
+uint32_t tempoTime = 500000;
+uint32_t controlTime = 2000;
+uint32_t triggerTime;
+bool triggered;
+
 uint16_t controlStepMax = 3;
 uint16_t controlStep;
 volatile bool ctrl;
@@ -14,7 +20,7 @@ uint16_t sequence1GateTimeMax =128; // Read from pot 0-255 : how long the gate i
 uint8_t tempo1Step;
 volatile bool sequencer1Trigger;
 volatile bool sequencer1Triggered;
-uint8_t tempo1StepMax = 1;  // Read from pot 15-0: sequence 1 tempo, relative to the main tempo
+uint8_t tempo1TimeMultiplier = 7;  // Read from pot 15-0: sequence 1 tempo, relative to the main tempo
 volatile bool tempo1StepOut;
 uint8_t sequence1Step;
 uint8_t sequence1LastStep = 15; // Read from pot 0-15 : how much of the sequence is played
@@ -29,7 +35,7 @@ uint16_t sequence2GateTimeMax =128; // Read from pot 0-255 : how long the gate i
 uint8_t tempo2Step;
 volatile bool sequencer2Trigger;
 volatile bool sequencer2Triggered;
-uint8_t tempo2StepMax = 3;  // Read from pot 15-0: sequence 1 tempo, relative to the main tempo
+uint8_t tempo2TimeMultiplier = 3;  // Read from pot 15-0: sequence 1 tempo, relative to the main tempo
 volatile bool tempo2StepOut;
 uint8_t sequence2Step;
 uint8_t sequence2LastStep = 15; // Read from pot 0-15 : how much of the sequence is played
@@ -49,18 +55,6 @@ void setup() {
 
   TCCR1B = TCCR1B & B11111000 | B00000001;
 
-  TCCR2A = 0;// set entire TCCR2A register to 0
-  TCCR2B = 0;// same for TCCR2B
-  TCNT2  = 0;//initialize counter value to 0
-  // set compare match register for 8khz increments
-  OCR2A = 249;// = (16*10^6) / (2000*8) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR2A |= (1 << WGM21);
-  // Set CS21 bit for 32 prescaler
-  TCCR2B |= (1 << CS20)|(1 << CS21);   
-  // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
-
   sei();
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
@@ -76,53 +70,36 @@ void setup() {
   Serial.begin(9600);
 }
 
-ISR(TIMER2_COMPA_vect) {
-  if (steppStep < steppStepMax) {
-    steppStep++;
-  } else {
-    stepp();
-    steppStep = 0;
-  }
-  if (controlStep < controlStepMax) {
-    controlStep++;
-  } else {
-    ctrl = true;
-    controlStep = 0;
-  }
-  ctrlFast = true;
-}
 
 void loop() {
-  //Serial.println("loop");
-  if (ctrl) {
+  newTime = micros();
+  if (digitalRead(12) && !triggered) {
+    if (triggerTime) {
+      stepp();
+      tempoTime = newTime - triggerTime;
+    }
+    triggerTime = newTime
+    triggered = true;
+  } else {
+    tiggered = false;
+  }
+
+  uint32_t tempo1Scaler = tempoTime*((tempo1TimeMultiplier+1)/8);
+  if (newTime> (triggerTime + tempo1Scaler)){
+    sequence1Stepp();
+  }
+  uint32_t tempo1Scaler = tempoTime*((tempo2TimeMultiplier+1)/8);
+  if (newTime> (triggerTime + tempo2Scaler)){
+    sequence2Stepp();
+  }
+  
+  if (newTime> (currentTime + controlTime)){
+    currentTime = newTime;
     control();
-    ctrl = false;
   }
-  if (ctrlFast) {
-    controlFast();
-    ctrlFast = false;
-  }
+  
 }
-void controlFast() {
-  sequencer1Trigger = tempo1StepOut;
-  if (sequencer1Trigger) {
-    if (!sequencer1Triggered) {
-      sequencer1Triggered = true;
-      sequence1Stepp();
-    }
-  } else {
-    sequencer1Triggered = false;
-  }
-  sequencer2Trigger = tempo2StepOut;
-  if (sequencer2Trigger) {
-    if (!sequencer2Triggered) {
-      sequencer2Triggered = true;
-      sequence2Stepp();
-    }
-  } else {
-    sequencer2Triggered = false;
-  }
-}
+
 void control() {
   //Serial.println(sequence1Step);
   sequence1GateState = sequence1GateTime < map(sequence1GateTimeMax, 0, 255, 0, (steppStepMax*(1 + tempo1StepMax))/controlStepMax);
@@ -166,31 +143,12 @@ void updateRegistersSequence2(uint8_t value) {
 }
 
 void stepp() {
-  /*
-  Serial.print(sequence1Step);
-  Serial.print("   ");
-  Serial.println(sequence2Step);
-  */
   if (mainTempoStep < 7) {
     digitalWrite(13, 0);
     mainTempoStep++;
   } else {
     digitalWrite(13, 1);
     mainTempoStep = 0;
-  }
-  if (tempo1Step < tempo1StepMax) {
-    tempo1StepOut = false;
-    tempo1Step++;
-  } else {
-    tempo1StepOut = true;
-    tempo1Step = 0;
-  }
-  if (tempo2Step < tempo2StepMax) {
-    tempo2StepOut = false;
-    tempo2Step++;
-  } else {
-    tempo2StepOut = true;
-    tempo2Step = 0;
   }
 }
 void sequence1NextStepForward() {
