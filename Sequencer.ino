f81bf2168d2ce39762cd86189e5bf21720d88e53
kellerdevsource <kellerdevgames@gmail.com>
2022-06-21 22:24:11 +0300

    Update Sequencer.ino

--- Sequencer.ino
+++ Sequencer.ino
@@ -1,10 +1,4 @@
 
-uint32_t newTime;
-uint32_t tempoTime = 500000;
-uint32_t controlTime = 2000;
-uint32_t triggerTime;
-bool triggered;
-
 uint16_t controlStepMax = 3;
 uint16_t controlStep;
 volatile bool ctrl;
@@ -12,21 +6,21 @@ volatile bool ctrlFast;
 uint16_t steppStepMax = 125; // The Tempo ~0-1024
 uint16_t steppStep;
 
-bool sequence1Forward = false;
-bool sequence2Forward = false;
+bool sequence1Forward = true;
+bool sequence2Forward = true;
 
 uint16_t sequence1GateTime;
-uint16_t sequence1GateTimeMax =128; // Read from pot 0-255 : how long the gate is open withing a step
+uint16_t sequence1GateTimeMax =255; // Read from pot 0-255 : how long the gate is open withing a step
 uint8_t tempo1Step;
 volatile bool sequencer1Trigger;
 volatile bool sequencer1Triggered;
-uint8_t tempo1TimeMultiplier = 7;  // Read from pot 15-0: sequence 1 tempo, relative to the main tempo
+uint8_t tempo1StepMax = 1;  // Read from pot 15-0: sequence 1 tempo, relative to the main tempo
 volatile bool tempo1StepOut;
 uint8_t sequence1Step;
-uint8_t sequence1LastStep = 15; // Read from pot 0-15 : how much of the sequence is played
+uint8_t sequence1LastStep = 3; // Read from pot 0-15 : how much of the sequence is played
 uint8_t sequence1FirstStep = 0;// Read from pot 0-15 : how much of the sequence is played
 uint8_t sequence1CV;
-bool sequence1SkipOffSteps = true;
+bool sequence1SkipOffSteps = false;
 bool sequence1Gate;
 bool sequence1GateState;
 
@@ -35,13 +29,13 @@ uint16_t sequence2GateTimeMax =128; // Read from pot 0-255 : how long the gate i
 uint8_t tempo2Step;
 volatile bool sequencer2Trigger;
 volatile bool sequencer2Triggered;
-uint8_t tempo2TimeMultiplier = 3;  // Read from pot 15-0: sequence 1 tempo, relative to the main tempo
+uint8_t tempo2StepMax = 3;  // Read from pot 15-0: sequence 1 tempo, relative to the main tempo
 volatile bool tempo2StepOut;
 uint8_t sequence2Step;
 uint8_t sequence2LastStep = 15; // Read from pot 0-15 : how much of the sequence is played
 uint8_t sequence2FirstStep = 0;// Read from pot 0-15 : how much of the sequence is played
 uint8_t sequence2CV;
-bool sequence2SkipOffSteps = true;
+bool sequence2SkipOffSteps = false;
 bool sequence2Gate;
 bool sequence2GateState;
 
@@ -54,6 +48,18 @@ void setup() {
   cli();
 
   TCCR1B = TCCR1B & B11111000 | B00000001;
+
+  TCCR2A = 0;// set entire TCCR2A register to 0
+  TCCR2B = 0;// same for TCCR2B
+  TCNT2  = 0;//initialize counter value to 0
+  // set compare match register for 8khz increments
+  OCR2A = 249;// = (16*10^6) / (2000*8) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR2A |= (1 << WGM21);
+  // Set CS21 bit for 32 prescaler
+  TCCR2B |= (1 << CS20)|(1 << CS21);   
+  // enable timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A);
 
   sei();
   pinMode(2, OUTPUT);
@@ -70,36 +76,53 @@ void setup() {
   Serial.begin(9600);
 }
 
-
-void loop() {
-  newTime = micros();
-  if (digitalRead(12) && !triggered) {
-    if (triggerTime) {
-      stepp();
-      tempoTime = newTime - triggerTime;
-    }
-    triggerTime = newTime
-    triggered = true;
+ISR(TIMER2_COMPA_vect) {
+  if (steppStep < steppStepMax) {
+    steppStep++;
   } else {
-    tiggered = false;
+    stepp();
+    steppStep = 0;
   }
-
-  uint32_t tempo1Scaler = tempoTime*((tempo1TimeMultiplier+1)/8);
-  if (newTime> (triggerTime + tempo1Scaler)){
-    sequence1Stepp();
+  if (controlStep < controlStepMax) {
+    controlStep++;
+  } else {
+    ctrl = true;
+    controlStep = 0;
   }
-  uint32_t tempo1Scaler = tempoTime*((tempo2TimeMultiplier+1)/8);
-  if (newTime> (triggerTime + tempo2Scaler)){
-    sequence2Stepp();
-  }
-  
-  if (newTime> (currentTime + controlTime)){
-    currentTime = newTime;
-    control();
-  }
-  
+  ctrlFast = true;
 }
 
+void loop() {
+  //Serial.println("loop");
+  if (ctrl) {
+    control();
+    ctrl = false;
+  }
+  if (ctrlFast) {
+    controlFast();
+    ctrlFast = false;
+  }
+}
+void controlFast() {
+  sequencer1Trigger = tempo1StepOut;
+  if (sequencer1Trigger) {
+    if (!sequencer1Triggered) {
+      sequencer1Triggered = true;
+      sequence1Stepp();
+    }
+  } else {
+    sequencer1Triggered = false;
+  }
+  sequencer2Trigger = tempo2StepOut;
+  if (sequencer2Trigger) {
+    if (!sequencer2Triggered) {
+      sequencer2Triggered = true;
+      sequence2Stepp();
+    }
+  } else {
+    sequencer2Triggered = false;
+  }
+}
 void control() {
   //Serial.println(sequence1Step);
   sequence1GateState = sequence1GateTime < map(sequence1GateTimeMax, 0, 255, 0, (steppStepMax*(1 + tempo1StepMax))/controlStepMax);
@@ -143,12 +166,31 @@ void updateRegistersSequence2(uint8_t value) {
 }
 
 void stepp() {
+  /*
+  Serial.print(sequence1Step);
+  Serial.print("   ");
+  Serial.println(sequence2Step);
+  */
   if (mainTempoStep < 7) {
     digitalWrite(13, 0);
     mainTempoStep++;
   } else {
     digitalWrite(13, 1);
     mainTempoStep = 0;
+  }
+  if (tempo1Step < tempo1StepMax) {
+    tempo1StepOut = false;
+    tempo1Step++;
+  } else {
+    tempo1StepOut = true;
+    tempo1Step = 0;
+  }
+  if (tempo2Step < tempo2StepMax) {
+    tempo2StepOut = false;
+    tempo2Step++;
+  } else {
+    tempo2StepOut = true;
+    tempo2Step = 0;
   }
 }
 void sequence1NextStepForward() {
@@ -176,6 +218,8 @@ void sequence1Stepp() {
         sequence1NextStepBackword();
       }
       if (sequence1Gate) {
+        sequence1CV = analogRead(20) >> 4;
+        analogWrite(9, sequence1CV<<2); 
         break;
       }
     }
@@ -185,9 +229,11 @@ void sequence1Stepp() {
       } else {
         sequence1NextStepBackword();
       }
+      if (sequence1Gate) {
+          sequence1CV = analogRead(20) >> 4;
+          analogWrite(9, sequence1CV<<2); 
+     }
   }
-  sequence1CV = analogRead(20) >> 4;
-  analogWrite(9, sequence1CV<<2);
   sequence1GateTime = 0;
 }
 void sequence2NextStepForward() {
